56d2fcd3c9edc3e98cba129d18260ab99e064c19
raharm <rahamoosavi1378@gmail.com>
2023-08-30 10:09:53 +0200

    up

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin', admin.site.urls),
     path('admin/', admin.site.urls),
+    # path('admin', admin.site.urls),
 
     # apps
     path('', include('get_link.urls')),
+    # path('r/', include('redirect_link.urls')),
 ]
