f7b46083cede3c51dea74260c71480677e5504ab
raharm <rahamoosavi1378@gmail.com>
2023-08-30 08:50:37 +0200

    edit view for forms

--- get_link/views.py
+++ get_link/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .forms import FormGetLink
+
 
 # Create your views here.
 
 def index(request):
-    return render(request,template_name='get_link/index.html')
+    if request.method == 'POST':
+        f = FormGetLink(request.POST)
+        if f.is_valid():
+            print(f.cleaned_data)
+        else:
+            return render(request, template_name='get_link/index.html', context={
+                'has_errors': True
+            })
+
+    return render(request, template_name='get_link/index.html', context={
+        'has_errors': False
+    })
