6062bbb4dca62a0c33423f205ae3e311df0764f2
raharm <rahamoosavi1378@gmail.com>
2023-08-30 07:26:09 +0200

    remove verbose_name in models

--- get_link/models.py
+++ get_link/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Links(models.Model):
-    user_input_link = models.URLField(verbose_name='لینک ورودی کاربر')
-    short_link = models.URLField(verbose_name='لینک ورودی کاربر')
+    user_input_link = models.URLField()
+    short_link = models.URLField()
