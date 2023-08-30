e81f24cbef05d988ac01a8ee9046a9e21cecca7e
raharm <rahamoosavi1378@gmail.com>
2023-08-30 07:54:38 +0200

    url: "admin" = "admin/"

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('admin', admin.site.urls),
     path('admin/', admin.site.urls),
+
+    # apps
     path('', include('get_link.urls')),
 ]
