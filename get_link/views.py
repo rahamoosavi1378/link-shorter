bb9c6a8e6440a5ec77015f3b26964101b7cfc1a7
raharm <rahamoosavi1378@gmail.com>
2023-08-30 10:17:54 +0200

    update views : - add notification success - add models Links for save data - add validetor url from user

--- get_link/views.py
+++ get_link/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from .forms import FormGetLink
-
+from .models import Links
 
 # Create your views here.
 
 def index(request):
+    context = {
+        'has_errors': False,
+        'has_success': False,
+    }
     if request.method == 'POST':
         f = FormGetLink(request.POST)
         if f.is_valid():
-            print(f.cleaned_data)
+            context['has_success'] = True
+            l = Links(user_input_link=f.cleaned_data['url_address'])
+            l.save()
         else:
-            return render(request, template_name='get_link/index.html', context={
-                'has_errors': True
-            })
+            context['has_errors'] = True
+            return render(request, template_name='get_link/index.html', context=context)
 
-    return render(request, template_name='get_link/index.html', context={
-        'has_errors': False
-    })
+    return render(request, template_name='get_link/index.html', context=context)
