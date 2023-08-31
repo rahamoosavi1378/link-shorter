19b4d6ceef4744e9e247ef70293a0db32aedb105
raharm <rahamoosavi1378@gmail.com>
2023-08-31 23:36:21 +0200

    update : - chenge model field from URL to Charset -> hash Url -  add field is_active - def save

--- get_link/models.py
+++ get_link/models.py
@@ -2,19 +2,18 @@ from django.db import models
 from django.urls import reverse
 import hashlib
 
+
 # Create your models here.
 
 class Links(models.Model):
     user_input_link = models.URLField(null=False)
-    short_link = models.URLField(null=True,blank=True)
-    # is_active : default True
-    #
+    hash_link = models.CharField(max_length=32, null=False, default='',db_index=True)
+    is_active = models.BooleanField(default=True)
+    # date: start ~ expire
 
-    def save(self,*args,**kwargs):
-        result_hash = hashlib.md5(self.user_input_link.encode()).hexdigest()
-        self.short_link = f'http://127.0.0.1:8000/r/{result_hash}'
-        # self.short_link = reverse('get_link_page',args=[result_hashe])
-        super().save(*args,**kwargs)
+    def save(self, *args, **kwargs):
+        self.hash_link = hashlib.md5(self.user_input_link.encode()).hexdigest()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.user_input_link
