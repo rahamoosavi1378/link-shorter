54c6ebe3e63ce6a64480b5eb40d1ee64ac18d9b1
raharm <rahamoosavi1378@gmail.com>
2023-09-01 13:08:37 +0200

    Showing the primary restored link to the user and displaying a warning message for the user to pay more attention to the primary link

--- redirect_link/views.py
+++ redirect_link/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    HttpResponse,
+    redirect
+)
+
+from get_link.models import Links
+
 
 # Create your views here.
+
+def redirect_page(request):
+    return redirect('get_link_page')
+
+
+def redirect_hash(request, hash_redirect):
+    context = {
+        'info': Links.objects.get(hash_link=hash_redirect)
+    }
+
+    return render(request, template_name='redirect_link/index.html', context=context)
