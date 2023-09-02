03e65c7110528221735a1b2d94a3a1484631077f
raharm <rahamoosavi1378@gmail.com>
2023-09-02 21:25:33 +0200

    update models : remove hasher method

--- get_link/models.py
+++ get_link/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
-import hashlib
+
+
+# import hashlib
 
 
 # Create your models here.
@@ -9,15 +11,14 @@ class Links(models.Model):
     user_input_link = models.URLField(null=False)
     hash_link = models.CharField(max_length=32,
                                  null=False,
-                                 unique=True,
                                  db_index=True)
     is_active = models.BooleanField(default=True)
 
     # date: start ~ expire
 
-    def save(self, *args, **kwargs):
-        self.hash_link = hashlib.md5(self.user_input_link.encode()).hexdigest()
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.hash_link = hashlib.md5(self.user_input_link.encode()).hexdigest()
+    #     super().save(*args, **kwargs)
 
     def __str__(self):
         return self.user_input_link
