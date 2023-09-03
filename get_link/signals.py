7c61d153a0c9ca35eddbe7b20b760901a492b4f4
raharm <rahamoosavi1378@gmail.com>
2023-09-03 19:35:36 +0200

    update: get hash md5

--- get_link/signals.py
+++ get_link/signals.py
@@ -2,12 +2,12 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from .models import Links
 
-import hashlib
-
+# import hashlib
+from utils.hasher import MD5,GenSlug
 
 # pre_save signal using decorator
 
 @receiver(pre_save, sender=Links)
 def set_hash_link(sender, instance, **kwargs):
-    instance.hash_link = hashlib.md5(
-        instance.user_input_link.encode()).hexdigest()
+    instance.hash_link = MD5(instance.user_input_link).md5()
+    instance.slug = GenSlug()
