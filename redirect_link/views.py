933a8af0c926bda08bca8eb92b5672b232ca23f0
raharm <rahamoosavi1378@gmail.com>
2023-09-03 19:38:43 +0200

    up: view templates for redirect hash and slug

--- redirect_link/views.py
+++ redirect_link/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import (
     render,
-    redirect
+    redirect,
+    get_object_or_404,
 )
 
 from get_link.models import Links
@@ -14,10 +15,29 @@ def redirect_page(request):
 
 def redirect_hash(request, hash_redirect):
     context = {
-        'info': Links.objects.get(hash_link=hash_redirect)
+        # 'info': Links.objects.get(hash_link=hash_redirect),
+        'info': get_object_or_404(Links, hash_link=hash_redirect),
     }
 
-    return render(request,
-                  template_name='redirect_link/index.html',
-                  context=context
-                  )
+    return render(request, template_name='redirect_link/index.html',
+                  context=context)
+
+
+def redirect_hash_10_char(request, hash_r_10_char):
+    context = {
+        # 'info': Links.objects.get(hash_link__startswith=hash_r_10_char),
+        'info': get_object_or_404(Links, hash_link__startswith=hash_r_10_char),
+    }
+
+    return render(request, template_name='redirect_link/index.html',
+                  context=context)
+
+
+def redirect_hash_slug(request, hash_r_slug):
+    context = {
+        # 'info': Links.objects.get(slug=hash_r_slug),
+        'info': get_object_or_404(Links, slug=hash_r_slug),
+    }
+
+    return render(request, template_name='redirect_link/index.html',
+                  context=context)
