b8eacd543f674fb1942e051a7bb1eba27ee015bf
raharm <rahamoosavi1378@gmail.com>
2023-09-03 19:41:06 +0200

    up: views for redirect hash and slug

--- get_link/views.py
+++ get_link/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, reverse
 from .forms import FormGetLink
 from .models import Links
-import hashlib
+# import hashlib
+
+from utils.hasher import MD5
 
 
 # Create your views here.
@@ -11,6 +13,8 @@ def index(request):
         'has_errors': False,
         'has_success': False,
         'redirect_link': '',
+        'redirect_link_10_char': '',
+        'redirect_link_slug': '',
     }
 
     if request.method == 'POST':
@@ -18,18 +22,28 @@ def index(request):
         if f.is_valid():
             context['has_success'] = True
             url_address = f.cleaned_data['url_address']
-            url_hashed = hashlib.md5(url_address.encode()).hexdigest()
-            if not Links.objects.filter(hash_link=url_hashed).exists():
+            url_hashed = MD5(url_address).md5().__str__()
+
+            obj = Links.objects
+            if not obj.filter(hash_link=url_hashed).exists():
                 Links(user_input_link=url_address).save()
 
             context['redirect_link'] = request.build_absolute_uri(
                 reverse('hash_redirect', args=[url_hashed])
             )
 
+            context['redirect_link_10_char'] = request.build_absolute_uri(
+                reverse('hash_r_10_char', args=[url_hashed[:10]])
+            )
+
+            slug = obj.get(hash_link=url_hashed).slug
+
+            context['redirect_link_slug'] = request.build_absolute_uri(
+                reverse('hash_r_slug', args=[slug])
+            )
+
         else:
             context['has_errors'] = True
-            return render(request, template_name='get_link/index.html',
-                          context=context)
 
     return render(request, template_name='get_link/index.html',
                   context=context)
